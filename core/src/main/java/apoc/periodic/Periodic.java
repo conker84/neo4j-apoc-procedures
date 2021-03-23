63245346ce619aa705c680859704809c684f05ff
Andrea Santurbano <santand@gmail.com>
2021-03-23 17:33:45 +0100

    new commit

--- core/src/main/java/apoc/periodic/Periodic.java
+++ core/src/main/java/apoc/periodic/Periodic.java
@@ -247,7 +247,7 @@ public class Periodic {
             @Name("cypherIterate") String cypherIterate,
             @Name("cypherAction") String cypherAction,
             @Name("config") Map<String,Object> config) {
-        log.info("Executing iterate");
+        log.info("Executing iterate 1");
         validateQuery(cypherIterate);
 
         long batchSize = Util.toLong(config.getOrDefault("batchSize", 10000));
